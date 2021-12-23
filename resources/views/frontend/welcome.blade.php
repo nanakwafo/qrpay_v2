60dbfff41009845e2e24e2b456a94991a77df67b
nanakwafo <nanamensah1140@gmail.com>
2021-12-23 07:59:21 +0000

    fix links on header

--- resources/views/frontend/welcome.blade.php
+++ resources/views/frontend/welcome.blade.php
@@ -62,19 +62,7 @@
             <a href="template/classic/dashboard-one.html" class="btn btn-white btn-uppercase flex-fill mg-l-10">Explore Demo</a>
         </div>
 
-        <div class="d-flex tx-20 mg-t-40">
-            <div class="tx-purple"><i class="fab fa-bootstrap"></i></div>
-            <div class="tx-orange pd-l-10"><i class="fab fa-html5"></i></div>
-            <div class="tx-primary pd-l-10"><i class="fab fa-css3-alt"></i></div>
-            <div class="tx-pink pd-l-10"><i class="fab fa-sass"></i></div>
-            <div class="tx-warning pd-l-10"><i class="fab fa-js"></i></div>
-            <div class="tx-danger pd-l-10"><i class="fab fa-npm"></i></div>
-            <div class="tx-danger pd-l-10"><i class="fab fa-gulp"></i></div>
-            <div class="bd-l mg-l-10 mg-sm-l-20 pd-l-10 pd-sm-l-20"></div>
-            <div class="tx-color-03" data-toggle="tooltip" data-title="Ongoing development"><i class="fab fa-angular"></i></div>
-            <div class="tx-color-03 pd-l-10" data-toggle="tooltip" data-title="Coming soon"><i class="fab fa-react"></i></div>
-            <div class="tx-color-03 pd-l-10" data-toggle="tooltip" data-title="Coming soon"><i class="fab fa-vuejs"></i></div>
-        </div>
+
 
         <div class="tx-12 mg-t-40">
             <a href="docs.html" class="link-03">Doc<span class="d-none d-sm-inline">umentation</span><span class="d-sm-none">s</span></a>
